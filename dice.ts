826ac2202b7428ede9921f205f072ef1ae5e65a9
Nyarlathotep <Nyarlathotep@protonmail.com>
2023-03-05 10:20:50 -0500

    Small code cleanup

---
+++ dice.ts
@@ -0,0 +1,28 @@
+export function makeRoll(
+  count: number,
+  sides: number,
+  ex: number,
+  rote: boolean,
+): Array<number> {
+  const rolls: number[] = [];
+
+  for (let i = 0; i < count; i++) {
+    const res = rollDie(sides);
+    rolls.push(res);
+    if (res >= ex) {
+      rolls.push(makeRoll(1, sides, ex, false)[0]);
+    } else if (res < 8 && rote) {
+      rolls.push(makeRoll(1, sides, ex, false)[0]);
+    }
+  }
+  return rolls;
+}
+
+function rollDie(sides: number): number {
+  const roll = new Uint8Array(1);
+  crypto.getRandomValues(roll);
+  if (roll[0] >= Math.floor(256 / sides) * sides) {
+    return rollDie(sides);
+  }
+  return 1 + (roll[0] % sides);
+}
