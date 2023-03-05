331a1c503c4a1c6d2b3b059df865ffa5cf3c04b3
Nyarlathotep <Nyarlathotep@protonmail.com>
2023-03-05 07:54:14 -0500

    Added some nice formatting and tightened up the regex

--- shoutbot.ts
+++ shoutbot.ts
@@ -11,12 +11,14 @@ const bot = createBot({
       console.log("Successfully connected to gateway");
     },
     messageCreate(bot, message) {
-      const capture = /^!([0-9]{1,3})d([0-9]{1,3})(a([0-9]))?(r)?/.exec(message.content);
+      const capture = /^!([0-9]{1,2})d([0-9]{1,2})(a([0-9]))?(r)?/.exec(message.content);
       if(capture) {
         const count = capture[1];
         const sides = capture[2];
         let ex = 10;
         let rote = false;
+        let rollmsg = `<@${message.member?.id}> just rolled [`;
+
         if(capture[4]){
           ex = +capture[4];
         } else {
@@ -32,11 +34,21 @@ const bot = createBot({
         }
         const rolled = makeRoll(+count, +sides, ex, rote);
         for( let i =0; i< rolled.length; i++) {
-          if(rolled[i] >= 8) {
+          if(rolled[i] >= ex) {
+            rollmsg += `\**${rolled[i]}**`;
             successes++;
+          } else if(rolled[i] >= 8) {
+            rollmsg += `${rolled[i]}`;
+            successes++;
+          }else {
+            rollmsg += `~~${rolled[i]}~~`;
+          }
+          if(i != rolled.length - 1) {
+            rollmsg += ','
           }
         }
-        sendMessage(bot, message.channelId,{content:`<@${message.member?.id}> just rolled ${rolled} for ${successes} successes.`});
+        rollmsg += `] for ${successes} successes.`;
+        sendMessage(bot, message.channelId,{content: rollmsg});
 
       }
 
