f0dd0b342dbde0c238b677cde74b54e02340c00f
Nyarlathotep <Nyarlathotep@protonmail.com>
2023-03-05 12:53:22 -0500

    Forgot a few things

--- mod.ts
+++ mod.ts
@@ -1,11 +1,9 @@
 import {
   createBot,
   Intents,
-  sendMessage,
   startBot,
 } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
-import { makeRoll } from "./dice.ts";
 import { handleMessage } from "./handler.ts";
 
 const bot = createBot({
